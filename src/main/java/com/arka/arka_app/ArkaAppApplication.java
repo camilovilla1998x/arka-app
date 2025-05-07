43520d4e42626126a28fdf0c60ad8c8b01db611a
Camilo Villa Agudelo <cami.villa13@hotmail.com>
2025-05-06 19:35:50 -0500

    Primer commit, inicializo el proyecto Arka

---
+++ src/main/java/com/arka/arka_app/ArkaAppApplication.java
@@ -0,0 +1,13 @@
+package com.arka.arka_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arka/arka_app/ArkaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arka.arka_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
